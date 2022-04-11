0051f28c610617f9f4fe14ae6981ea59df9bb6ed
Shahriar Rifat <shahriarrifat0@gmail.com>
2022-04-11 22:50:24 +0600

    Complete rebuild

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Colors.grey.shade900,
+        backgroundColor: Colors.black,
         centerTitle: true,
         title: Text(
-          'Random Birds Chirping',
-          style: TextStyle(),
+          'Some Game Sounds',
+          style: TextStyle(
+            fontFamily: 'GrapeNuts',
+            fontSize: 30.0,
+          ),
         ),
       ),
       body: Xylophone2(),
@@ -20,8 +25,41 @@ void main() {
 class Xylophone2 extends StatelessWidget {
   const Xylophone2({Key? key}) : super(key: key);
 
+  void playSound({required int soundNumber}) async {
+    final player = AudioCache();
+    player.play('$soundNumber.opus');
+  }
+
+  Expanded buildImageButton({required int imageNumber, required int soundNumber}) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 5),
+        child: Ink.image(
+          image: AssetImage('images/$imageNumber.jpg'),
+          fit: BoxFit.cover,
+          child: InkWell(
+              enableFeedback: false,
+              onTap: () {
+                playSound(soundNumber: soundNumber);
+              }),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        buildImageButton(imageNumber: 1, soundNumber: 1),
+        buildImageButton(imageNumber: 2, soundNumber: 2),
+        buildImageButton(imageNumber: 3, soundNumber: 3),
+        buildImageButton(imageNumber: 4, soundNumber: 4),
+        buildImageButton(imageNumber: 5, soundNumber: 5),
+        buildImageButton(imageNumber: 6, soundNumber: 6),
+        buildImageButton(imageNumber: 7, soundNumber: 7),
+      ],
+    );
   }
 }
